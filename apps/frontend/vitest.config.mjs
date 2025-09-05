b603a436bfea658d4f7822af2741fafa24f4c7f2
Rob Denison <rob@blim.org>
2025-09-05 14:21:26 +0100

    Added first iteration of vitest integration - added core functionality, no tests

---
+++ apps/frontend/vitest.config.mjs
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config'
+import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['setupTests.ts'],
+    globals: true,
+    include: ['**/*.{test,spec}.{ts,tsx}'],
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, '.'),
+    },
+  },
+})
